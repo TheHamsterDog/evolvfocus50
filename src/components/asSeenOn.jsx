7390d4f729671358f7f25acb1ad23c81f01063ef
Shreshth Verma <shreshth@shreshthverma.me>
2022-04-17 14:11:47 +0530

    feat: fixed numerous sections

--- src/components/asSeenOn.jsx
+++ src/components/asSeenOn.jsx
@@ -1,8 +1,12 @@
-const AsSeenOn = () =>{
-    return(<div className="news">
-        <h1>As seen on leading national and international media</h1>
-        <img src="/as-seen-on.jpg" alt="news"/>
-    </div>)
+const AsSeenOn = () => {
+    return (<div className="news">
+        <div className="header">
+            <h1>As seen on leading national and international media</h1>
+        </div>
+        <img src="/as-seen-on.jpg" alt="news" />
+    </div>
+
+    )
 
 }
 export default AsSeenOn;
